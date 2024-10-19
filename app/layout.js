c6c5e6c970d242fbc98962b9f64cea212b852fd9
anzi15 <djam4343@gmail.com>
2024-10-19 15:53:50 +0500

    added meta pixel

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import HeaderMsg from './components/HeaderMsg';
 import Footer from './components/Footer';
 import ToastProvider from './components/ToastProviderProps';
 import { usePathname } from 'next/navigation';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { doc, getDoc } from "firebase/firestore"
 import { db } from './lib/firebase/firbaseConfig';
@@ -39,6 +40,33 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+            <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1203417130932778');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1203417130932778&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <body className="antialiased" suppressHydrationWarning={true}>
         <ToastProvider>
           {!hideHeaderFooter && (
